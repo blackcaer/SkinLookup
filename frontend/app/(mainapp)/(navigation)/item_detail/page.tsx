4da56abba2a4f88eb613769b1d3d106f813b490d
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-12-21 23:31:34 +0100

    feat: Added chart-volume

--- frontend/app/(mainapp)/(navigation)/item_detail/page.tsx
+++ frontend/app/(mainapp)/(navigation)/item_detail/page.tsx
@@ -5,6 +5,7 @@ import { ItemInfo } from "@/app/ui/types";
 import ChartPrice from "@/app/ui/stats/chart-price";
 
 import { demo1, demo2 } from "@/app/ui/demo-data";
+import ChartVolume from "@/app/ui/stats/chart-volume";
 
 export default function Page() {
   const item = demo2;
@@ -31,38 +32,35 @@ export default function Page() {
   const filteredPhsm = item.phsm.map(({ volume, ...rest }) => rest);
 
   return (
-    <div > {/*className="flex flex-col sm:flex-col gap-1 md:overflow-hidden">*/}
+    <div>
+      {" "}
+      {/*className="flex flex-col sm:flex-col gap-1 md:overflow-hidden">*/}
+      <div className="flex flex-col gap-2">
+        <div className="flex flex-row gap-4">
+          <Link href="" className="flex flex-row">
+            <Image
+              src={item.previewUrl || "/default.png"}
+              alt={`${item.name} picture`}
+              className="mr-4 rounded-md"
+              width={400}
+              height={400}
+            />
+          </Link>
 
-        <div className="flex flex-col gap-2">
-            <div className="flex flex-row gap-4">
-                <Link href="" className="flex flex-row">
-                    <Image
-                    src={item.previewUrl || "/default.png"}
-                    alt={`${item.name} picture`}
-                    className="mr-4 rounded-md"
-                    width={400}
-                    height={400}
-                    />
-                </Link>
-
-                <div className="flex flex-col gap-2">
-                    <h2 > {item.name} </h2>
-                    <p > Collection {item.itemCollection} </p>
-                    <p > itemType {item.itemType} </p>
-                    <p > Estimated supply on market {item.supplyTotalEstimated} </p>
-                    <p > Accepted {item.timeAccepted} </p>
-                    <p > Steam Store price {item.storePrice} </p>
-                </div>
-            </div>
-
-                
-            <ChartPrice 
-                data={filteredPhsm} 
-                config={barChartConfig}
-                />
-
+          <div className="flex flex-col gap-2">
+            <h2> {item.name} </h2>
+            <p> Collection {item.itemCollection} </p>
+            <p> itemType {item.itemType} </p>
+            <p> Estimated supply on market {item.supplyTotalEstimated} </p>
+            <p> Accepted {item.timeAccepted} </p>
+            <p> Steam Store price {item.storePrice} </p>
+          </div>
         </div>
 
+        <ChartPrice data={item.phsm} config={barChartConfig} />
+
+        <ChartVolume data={item.phsm} config={barChartConfig} />
+      </div>
     </div>
   );
 }
---
+++ frontend/app/ui/stats/chart-volume.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import React from "react";
+import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  XAxis,
+  YAxis,
+  Tooltip as ChartTooltip,
+} from "recharts";
+import { format } from "date-fns";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+
+interface ChartPriceProps {
+  data: { date: string; volume: number }[];
+  config: ChartConfig;
+  title?: string;
+  description?: string;
+}
+
+export default function ChartVolume({
+  data,
+  config,
+  title,
+  description,
+}: ChartPriceProps) {
+  title = title || "Volume over time";
+  description =
+    description || "Volume of an item on Steam Market since item's release";
+  return (
+    <Card className="w-full">
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{description}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={config}>
+          <LineChart
+            data={data}
+            margin={{
+              top: 5,
+              right: 5,
+              left: 0,
+              bottom: 35,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="date" angle={-45} textAnchor="end" />
+            <YAxis />
+            <ChartTooltip content={<ChartTooltipContent />} />
+            <Line
+              type="monotone"
+              dataKey="volume"
+              stroke="var(--color-price)"
+              strokeWidth={2}
+              dot={false}
+            />
+          </LineChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+}
