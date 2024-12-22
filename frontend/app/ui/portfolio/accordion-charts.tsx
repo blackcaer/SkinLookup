9885c10d0c670ed8273021d9b4ae820493882f14
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-12-22 23:19:46 +0100

    feat: add accordion component with chart integration for portfolio data

---
+++ frontend/app/ui/portfolio/accordion-charts.tsx
@@ -0,0 +1,38 @@
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+  } from "@/components/ui/accordion"
+import ChartPrice from "../stats/chart-price"
+import { demo1, demo2 } from "@/app/ui/demo-data";
+import { ChartConfigVolMed } from "@/app/ui/chart-configs";
+import ChartVolume from "../stats/chart-volume";
+
+export function AccordionCharts() {
+    const item = demo2;     //TODO - add props, change to real data
+    return (
+      <Accordion type="single" collapsible className="w-full">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Show items value throughout the history</AccordionTrigger>
+          <AccordionContent>
+            <ChartPrice data={item.phsm} config={ChartConfigVolMed} className="" chartClassName=""/>
+          </AccordionContent>
+        </AccordionItem>
+        
+        <AccordionItem value="item-2">
+          <AccordionTrigger>Show items volume throughout the history</AccordionTrigger>
+          <AccordionContent>
+            <ChartVolume data={item.phsm} config={ChartConfigVolMed} className="" chartClassName=""/>
+          </AccordionContent>
+        </AccordionItem>
+
+        <AccordionItem value="item-3">
+          <AccordionTrigger>Some other info</AccordionTrigger>
+          <AccordionContent>
+            there is info
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    )
+  }
