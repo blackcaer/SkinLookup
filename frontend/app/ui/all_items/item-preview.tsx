782df40fd004b33c421f3930fc1131f285dda774
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-12-23 19:31:04 +0100

    feat: enhance ItemPreview component layout with improved styling and structure

--- frontend/app/ui/all_items/item-preview.tsx
+++ frontend/app/ui/all_items/item-preview.tsx
@@ -1,4 +1,3 @@
-import clsx from "clsx";
 import Image from "next/image";
 import { FC } from "react";
 import Link from "next/link";
@@ -10,7 +9,9 @@ interface ItemPreviewProps {
 
 const ItemPreview: FC<ItemPreviewProps> = ({ item }) => {
   return (
-    <Link href="/item_details" className="flex flex-col w-[240px]">
+    <div className="flex flex-col w-[240px] h-[300px] border border-gray-300 rounded-lg shadow-lg">
+    <Link href="/item_details" >
+    
       <Image
         src={item.previewUrl || "/default.png"}
         alt={`${item.name} picture`}
@@ -18,9 +19,9 @@ const ItemPreview: FC<ItemPreviewProps> = ({ item }) => {
         width={240}
         height={240}
       />
-      <h2 className="break-words text-center"> {item.name} </h2>
-      {/* <p> Collection {item.itemCollection} </p> */}
-    </Link>
+      </Link>
+      <h2 className="break-words text-center pt-1"> {item.name} </h2>
+    </div>
   );
 };
 
