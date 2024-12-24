839391ecd3c9a4199952ba8e762956c574d5f840
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-12-24 15:07:14 +0100

    feat: Updated about page

--- frontend/app/(mainapp)/(navigation)/about/page.tsx
+++ frontend/app/(mainapp)/(navigation)/about/page.tsx
@@ -1,12 +1,17 @@
 import {ProfileForm} from "@/app/ui/about/contact_form";
+import Link from "next/link";
+
 
 export default function Page() {
 
     return (
       <div className="flex flex-col md:flex-col gap-4 md:overflow-hidden">
-        <div>
-            <h1>Contact Us</h1>
-            <ProfileForm/>
+        <div className="flex flex-col  gap-4 ">
+            <h1>About</h1>
+            <p>This site is made for Harvard's CS50W course, for Final Project - "Capstone". </p>
+            <p><a href="https://cs50.harvard.edu/web/2020/projects/final/capstone/">https://cs50.harvard.edu/web/2020/projects/final/capstone/ </a></p>
+            <p><a href=">https://github.com/blackcaer/SkinLookup">https://github.com/blackcaer/SkinLookup </a></p>
+
         </div>
         
       </div>
