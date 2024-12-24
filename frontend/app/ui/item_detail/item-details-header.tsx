ea6b2274a9048d2a216220d32ccb30446e2db4c3
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-12-24 17:23:10 +0100

    Chore: Fixed problem with responsivity after recent changes to the header

--- frontend/app/ui/item_detail/item-details-header.tsx
+++ frontend/app/ui/item_detail/item-details-header.tsx
@@ -15,29 +15,35 @@ function StatDisplayer({ title, value }: { title: string; value: string }) {
 }
 
 export default function ItemDetailHeader({ item }: ItemDetailHeaderProps) {
-    return (
-    <div className="flex flex-row justify-center">
-        <Link href="" className="flex flex-row">
-          <Image
-            src={item.previewUrl || "/default.png"}
-            alt={`${item.name} picture`}
-            className="mr-4 rounded-lg shadow-xl"
-            width={400}
-            height={400}
+  return (
+    <div className="flex flex-row justify-center flex-wrap">
+      <Link href="" className="flex flex-row">
+        <Image
+          src={item.previewUrl || "/default.png"}
+          alt={`${item.name} picture`}
+          className="mr-4 rounded-lg shadow-xl"
+          width={400}
+          height={400}
+        />
+      </Link>
+
+      <div className="flex flex-col py-8 px-4 bg-gray-50 rounded-lg shadow-lg text-xl">
+        <h2 className="text-2xl font-bold mb-6">Item name: {item.name}</h2>
+
+        <div className="text-lg flex flex-col gap-2">
+          <StatDisplayer title="Collection" value={item.itemCollection} />
+          <StatDisplayer title="Item type" value={item.itemType} />
+          <StatDisplayer
+            title="Estimated supply"
+            value={item.supplyTotalEstimated.toString()}
           />
-        </Link>
-
-        <div className="flex flex-col py-8 px-4 bg-gray-50 rounded-lg shadow-lg text-xl">
-
-          <h2 className="text-2xl font-bold mb-6">Item name: {item.name}</h2>
-
-          <div className="text-lg flex flex-col gap-2">
-            <StatDisplayer title="Collection" value={item.itemCollection} />
-            <StatDisplayer title="Item type" value={item.itemType} />
-            <StatDisplayer title="Estimated supply" value={item.supplyTotalEstimated.toString()} />
-            <StatDisplayer title="Steam Store price" value={item.storePrice.toString()} />
-            <StatDisplayer title="Accepted" value={item.timeAccepted} />
-          </div>
+          <StatDisplayer
+            title="Steam Store price"
+            value={item.storePrice.toString()}
+          />
+          <StatDisplayer title="Accepted" value={item.timeAccepted} />
         </div>
-      </div>);
+      </div>
+    </div>
+  );
 }
