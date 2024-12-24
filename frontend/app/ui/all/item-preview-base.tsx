e53874bd50f5d5046cf4bb7fafd343c351362d0d
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-12-24 22:28:53 +0100

    Added skeleton to preview-base

--- frontend/app/ui/all/item-preview-base.tsx
+++ frontend/app/ui/all/item-preview-base.tsx
@@ -1,5 +1,9 @@
+'use client';
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+import Skeleton from "react-loading-skeleton";
+import 'react-loading-skeleton/dist/skeleton.css';
 
 interface ItemPreviewBaseProps {
     item: {
@@ -9,15 +13,25 @@ interface ItemPreviewBaseProps {
 }
 
 export default function ItemPreviewBase({ item }: ItemPreviewBaseProps) {
+    const [isLoading, setIsLoading] = useState(true);
+
+    const handleLoadingComplete = () => {
+        console.log("Image loaded");
+        setIsLoading(false);
+    };
+
     return (
         <>
             <Link href="/item_details">
+                {isLoading && <Skeleton height={240} width={240} className="absolute top-0 left-0 rounded-md" />}
                 <Image
                     src={item.previewUrl || "/default.png"}
                     alt={`${item.name} picture`}
-                    className="mr-4 rounded-md"
+                    className={`mr-4 rounded-md transition-opacity duration-300 ${isLoading ? 'opacity-0' : 'opacity-100'}`}
                     width={240}
                     height={240}
+                    onLoad={handleLoadingComplete}
+                    priority={true}
                 />
             </Link>
             <div className="flex flex-col items-center justify-center h-full w-full">
