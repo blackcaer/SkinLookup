5065df483116e57d4cf58bfc91a4be549f8f5963
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-12-24 17:38:20 +0100

    feat: integrate search parameters in item details page and clean up search page layout

--- frontend/app/(mainapp)/(navigation)/item_details/page.tsx
+++ frontend/app/(mainapp)/(navigation)/item_details/page.tsx
@@ -1,19 +1,25 @@
+"use client";
 import { demo1, demo2 } from "@/app/ui/demo-data";
 import ChartVolume from "@/app/ui/charts/chart-volume";
 import { ChartConfigVolMed } from "@/app/ui/chart-configs";
 import ChartPrice from "@/app/ui/charts/chart-price";
 import SearchBar from "@/app/ui/all/search-bar";
 import ItemDetailHeader from "@/app/ui/item_detail/item-details-header";
+import { useSearchParams } from "next/navigation";
 
 export default function Page() {
-  const item = demo2;
+  const searchParams = useSearchParams();
+  let name = searchParams.get("name");
+  if (!name || name.trim() === "") name = "Forest Raiders Locker"; // TODO random name
+
+  const item = demo1; // TODO fetch item by name
 
   return (
     <div>
       <div className="mb-6">
-        <SearchBar/>
+        <SearchBar />
       </div>
-      
+
       <div className="flex flex-col gap-6">
         <ItemDetailHeader item={item} />
 
--- frontend/app/(mainapp)/(navigation)/search/page.tsx
+++ frontend/app/(mainapp)/(navigation)/search/page.tsx
@@ -1,13 +1,11 @@
 import SearchBar from "@/app/ui/all/search-bar";
 
 export default function Page() {
-
   return (
-      <div className="flex flex-col justify-center items-center w-full h-full">
-        <div className="w-full mb-[200px]  lg:mb-[200px] mx-16">
-            <SearchBar/>
-        </div>
+    <div className="flex flex-col justify-center items-center w-full h-full">
+      <div className="w-full mb-[200px] mx-16">
+        <SearchBar />
       </div>
-
+    </div>
   );
 }
