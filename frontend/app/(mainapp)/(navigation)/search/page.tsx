6539374758b573310507c4b0198e4bc3b978e95f
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-12-24 17:20:02 +0100

    feat: added basic Search site

---
+++ frontend/app/(mainapp)/(navigation)/search/page.tsx
@@ -0,0 +1,13 @@
+import SearchBar from "@/app/ui/all/search-bar";
+
+export default function Page() {
+
+  return (
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="w-full mb-[200px]  lg:mb-[200px] mx-16">
+            <SearchBar/>
+        </div>
+      </div>
+
+  );
+}
