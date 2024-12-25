4009457e12293697008f8af1e12a37c910efef72
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-12-25 15:48:10 +0100

    feat: Register models  in admin

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Item, ItemData, PortfolioItem, User
 
-# Register your models here.
+admin.site.register(Item)
+admin.site.register(ItemData)
+admin.site.register(PortfolioItem)
+admin.site.register(User)
