0a66456903d54d220418682edc8d4d1d451bfa2e
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-12-26 15:05:13 +0100

    feat: working login and logout

--- frontend/app/(mainapp)/login/page.tsx
+++ frontend/app/(mainapp)/login/page.tsx
@@ -1,83 +1,104 @@
-'use client';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import React, { useState, useEffect } from 'react';
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useRouter } from "next/navigation";
+import React, { useState, useEffect } from "react";
 
 const LoginPage: React.FC = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const router = useRouter();
 
-    useEffect(() => {
-        const checkAuth = async () => {
-            const token = localStorage.getItem('access_token');
-            if (token) {
-                setIsAuthenticated(true);
-            }
-        };
-
-        checkAuth();
-    }, []);
-
-    const handleSubmit = async (event: React.FormEvent) => {
-        event.preventDefault();
-        setError('');
-
-        const response = await fetch('http://127.0.0.1:8000/api/token/', {
-            method: 'POST',
+  useEffect(() => {
+    const checkAuth = async () => {
+      const token = localStorage.getItem("access_token");
+      if (token) {
+        const response = await fetch(
+          "http://127.0.0.1:8000/api/is_authenticated/",
+          {
+            method: "GET",
             headers: {
-                'Content-Type': 'application/json',
+              Authorization: `Bearer ${token}`,
             },
-            body: JSON.stringify({ username, password }),
-        });
+          }
+        );
 
         if (response.ok) {
-            const data = await response.json();
-
-            localStorage.setItem('access_token', data.access);
-            localStorage.setItem('refresh_token', data.refresh);
-            setIsAuthenticated(true);
-            console.log('Login successful:', data);
+          setIsAuthenticated(true);
+          router.push("/all");
         } else {
-            const errorData = await response.json();
-            setError(errorData.detail || 'Login failed');
+          setIsAuthenticated(false);
         }
+      }
     };
 
-    return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-            {isAuthenticated ? (
-                <div>
-                    <h2>Welcome, you are logged in!</h2>
-                </div>
-            ) : (
-                <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', width: '300px' }}>
-                    <h2>Login</h2>
-                    {error && <p style={{ color: 'red' }}>{error}</p>}
-                    <label>
-                        Username:
-                        <Input
-                            type="text"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                            required
-                        />
-                    </label>
-                    <label>
-                        Password:
-                        <Input
-                            type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                        />
-                    </label>
-                    <Button type="submit">Login</Button>
-                </form>
-            )}
-        </div>
-    );
+    checkAuth();
+  }, [router]);
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    setError("");
+
+    const response = await fetch("http://127.0.0.1:8000/api/token/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      localStorage.setItem("access_token", data.access);
+      localStorage.setItem("refresh_token", data.refresh);
+      setIsAuthenticated(true);
+      console.log("Login successful");
+      router.push("/all");
+    } else {
+      const errorData = await response.json();
+      setError(errorData.detail || "Login failed");
+    }
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <form
+        onSubmit={handleSubmit}
+        style={{ display: "flex", flexDirection: "column", width: "300px" }}
+      >
+        <h2>Login</h2>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        <label>
+          Username:
+          <Input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Password:
+          <Input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+        <Button type="submit">Login</Button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
--- frontend/app/ui/nav/sidenav.tsx
+++ frontend/app/ui/nav/sidenav.tsx
@@ -1,10 +1,40 @@
+'use client'
 import Link from 'next/link';
 import NavLinks from '@/app/ui/nav/nav-links';
 import SomeLogo from '@/app/ui/nav/some-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
-//import { signOut } from '@/auth';
+import { useRouter } from 'next/navigation';
 
 export default function SideNav() {
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    console.log('logging out...');
+    const refreshToken = localStorage.getItem('refresh_token');
+    const accessToken = localStorage.getItem('access_token');
+  
+    if (refreshToken) {
+      const response = await fetch('http://127.0.0.1:8000/api/logout/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${accessToken}`, // Dodano nagłówek autoryzacji
+        },
+        body: JSON.stringify({ refresh: refreshToken }),
+      });
+  
+      if (response.ok) {
+        localStorage.removeItem('access_token');
+        localStorage.removeItem('refresh_token');
+        router.push('/login');
+      } else {
+        console.error('Logout failed with status:', response.status);
+      }
+    } else {
+      console.log('No refresh token found. User might not be logged in.');
+    }
+  };
+
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
@@ -18,12 +48,13 @@ export default function SideNav() {
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-xl bg-gray-50 md:block"></div>
-        <form>
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-xl bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-            <PowerIcon className="w-6" />
-            <div className="hidden md:block">Sign Out</div>
-          </button>
-        </form>
+        <button
+          onClick={handleLogout}
+          className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-xl bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+        >
+          <PowerIcon className="w-6" />
+          <div className="hidden md:block">Sign Out</div>
+        </button>
       </div>
     </div>
   );
