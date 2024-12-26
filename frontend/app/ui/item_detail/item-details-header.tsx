c75ba73b40214a16edc9f28a8ad0923190c0b6df
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-12-26 16:26:16 +0100

    feat: add skeleton for main image loading

--- frontend/app/ui/item_detail/item-details-header.tsx
+++ frontend/app/ui/item_detail/item-details-header.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
 import { ItemInfo } from "@/app/ui/common/types";
+import Skeleton from "react-loading-skeleton";
+import { useState } from "react";
 
 interface ItemDetailHeaderProps {
   item: ItemInfo;
@@ -15,15 +17,25 @@ function StatDisplayer({ title, value }: { title: string; value: string }) {
 }
 
 export default function ItemDetailHeader({ item }: ItemDetailHeaderProps) {
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <div className="flex flex-row justify-center flex-wrap">
       <Link href="" className="flex flex-row">
+        {isLoading && (
+          <Skeleton
+            height={400}
+            width={400}
+            className="absolute top-0 left-0 rounded-md"
+          />
+        )}
         <Image
           src={item.previewUrl || "/default.png"}
           alt={`${item.name} picture`}
           className="mr-4 rounded-lg shadow-xl"
           width={400}
           height={400}
+          onLoad={() => setIsLoading(false)}
         />
       </Link>
 
