908df4558b053ef8b699eeb09ce6941b9d6370b4
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-12-26 19:13:19 +0100

    feat: implemented frontend pagination in AllItemList component

--- frontend/app/ui/all/all-item-list.tsx
+++ frontend/app/ui/all/all-item-list.tsx
@@ -1,12 +1,25 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { ItemInfo, PItem } from "@/app/ui/common/types";
+import { ItemInfo } from "@/app/ui/common/types";
 import ItemPreview from "@/app/ui/all/item-preview";
+
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+
+const ITEMS_PER_PAGE = 36;
 
 const AllItemList = () => {
   const [items, setItems] = useState<ItemInfo[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+  const [currentPage, setCurrentPage] = useState<number>(1);
 
   useEffect(() => {
     const fetchItems = async () => {
@@ -23,17 +36,56 @@ const AllItemList = () => {
         setLoading(false);
       }
     };
-
     fetchItems();
   }, []);
 
   if (error) return <div>{error}</div>;
 
+  const totalPages = Math.ceil(items.length / ITEMS_PER_PAGE);
+  const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+  const endIndex = startIndex + ITEMS_PER_PAGE;
+  const currentItems = items.slice(startIndex, endIndex);
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
   return (
     <div className="flex flex-col flex-wrap sm:flex-row gap-4 md:overflow-hidden">
-      {items.map((item) => (
-        <ItemPreview item={item} />
+      {currentItems.map((item) => (
+        <ItemPreview key={item.nameId} item={item} />
       ))}
+      <div>
+        <Pagination>
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious onClick={handlePreviousPage} href="#" />
+            </PaginationItem>
+            {Array.from({ length: totalPages }, (_, index) => (
+              <PaginationItem key={index}>
+                <PaginationLink
+                  href="#"
+                  isActive={index + 1 === currentPage}
+                  onClick={() => setCurrentPage(index + 1)}
+                >
+                  {index + 1}
+                </PaginationLink>
+              </PaginationItem>
+            ))}
+            <PaginationItem>
+              <PaginationNext onClick={handleNextPage} href="#"/>
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      </div>
     </div>
   );
 };
