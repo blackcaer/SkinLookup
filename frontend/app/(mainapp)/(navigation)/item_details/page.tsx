189eaa360a7050e72a17ca82e53b5c7c12a5885b
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-12-27 19:00:07 +0100

    feat: implement dynamic item fetching in ItemDetailsPage with loading state

--- frontend/app/(mainapp)/(navigation)/item_details/page.tsx
+++ frontend/app/(mainapp)/(navigation)/item_details/page.tsx
@@ -1,18 +1,51 @@
 "use client";
-import { demo1, demo2 } from "@/app/ui/demo-data";
-import ChartVolume from "@/app/ui/charts/chart-volume";
-import { ChartConfigVolMed } from "@/app/ui/common/chart-configs";
-import ChartPrice from "@/app/ui/charts/chart-price";
+import React, { useEffect, useState } from 'react';
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import SearchBar from "@/app/ui/all/search-bar";
 import ItemDetailHeader from "@/app/ui/item_detail/item-details-header";
-import { useSearchParams } from "next/navigation";
+import ChartPrice from "@/app/ui/charts/chart-price";
+import ChartVolume from "@/app/ui/charts/chart-volume";
+import { ChartConfigVolMed } from "@/app/ui/common/chart-configs";
+import { ItemData } from '@/app/ui/common/types';
 
-export default function Page() {
+
+const ItemDetailsPage = () => {
+  const router = useRouter();
   const searchParams = useSearchParams();
   let name = searchParams.get("name");
-  if (!name || name.trim() === "") name = "Forest Raiders Locker"; // TODO random name
+  if (!name || name.trim() === "") router.push("/all");
 
-  const item = demo1; // TODO fetch item by name
+  const [itemData, setItemData] = useState<ItemData | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const getItemData = async () => {
+      try {
+        const response = await fetch(
+          `http://127.0.0.1:8000/api/items/?name=${name}`,
+          {
+            method: "GET",
+          }
+        );
+        if (response.ok) {
+          console.log("Response ok");
+          const data = await response.json();
+          console.log(data);
+          setItemData(data);
+        } else {
+          console.log("Response bad");
+          alert(`Error fetching item data: ${response.statusText} (${response.status})`);
+        }
+      } catch (error) {
+        console.error("Error fetching item data:", error);
+        alert("Error fetching item data");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getItemData();
+  }, [name]);
 
   return (
     <div>
@@ -21,12 +54,21 @@ export default function Page() {
       </div>
 
       <div className="flex flex-col gap-6">
-        <ItemDetailHeader item={item} />
-
-        <ChartPrice data={item.phsm} config={ChartConfigVolMed} />
-
-        <ChartVolume data={item.phsm} config={ChartConfigVolMed} />
+        {isLoading ? (
+          
+          <div className="w-full h-[800px] bg-gray-200 animate-pulse rounded-lg"></div>
+        ) : (
+          itemData && (
+            <>
+              <ItemDetailHeader item={itemData.item} />
+              <ChartPrice data={itemData.phsm} config={ChartConfigVolMed} />
+              <ChartVolume data={itemData.phsm} config={ChartConfigVolMed} />
+            </>
+          )
+        )}
       </div>
     </div>
   );
-}
+};
+
+export default ItemDetailsPage;
