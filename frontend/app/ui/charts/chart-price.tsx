4e4067f16e41f768f973b326ff98fd80d9915b19
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-12-27 18:58:49 +0100

    fix: update date formatting in ChartPrice and ChartVolume components to match api response

--- frontend/app/ui/charts/chart-price.tsx
+++ frontend/app/ui/charts/chart-price.tsx
@@ -33,9 +33,11 @@ interface ChartPriceProps {
 }
 
 const formatDate = (dateString: string) => {
-  const [day, month, year] = dateString.split('/');
-  const shortYear = year.slice(-2);
-  return `${day}/${month}/${shortYear}`;
+  const date = new Date(dateString);
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Miesiące są indeksowane od 0
+  const year = String(date.getFullYear()).slice(-2);
+  return `${day}/${month}/${year}`;
 };
 
 export default function ChartPrice({
