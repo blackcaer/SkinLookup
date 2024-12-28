19f452b239951fe233f9abe14819d34e4a6087c2
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-12-29 00:54:54 +0100

    feat: add user authentication check and pass login status to ItemPreview component

--- frontend/app/ui/all/all-item-list.tsx
+++ frontend/app/ui/all/all-item-list.tsx
@@ -17,6 +17,7 @@ const ITEMS_PER_PAGE = 36;
 
 const AllItemList = () => {
   const [items, setItems] = useState<ItemInfo[]>([]);
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
   const [namesInPortfolio, setNamesInPortfolio] = useState<string[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
@@ -24,14 +25,27 @@ const AllItemList = () => {
 
   useEffect(() => {
     const fetchItems = async () => {
+      const token = localStorage.getItem("access_token");
+      if (!token) {
+        console.log("No token found");
+      } else {
+        setIsLoggedIn(true);
+      }
+
       try {
-        const response = await fetch("http://127.0.0.1:8000/api/items/all/");
+        const response = await fetch("http://127.0.0.1:8000/api/items/all/", {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
         if (!response.ok) {
           throw new Error("Failed to fetch items");
         }
         const data = await response.json();
-        setNamesInPortfolio(data['names_in_portfolio']);
-        setItems(data['items']);
+
+        setNamesInPortfolio(data["names_in_portfolio"]); // If user not authenticated, this will be an empty array
+        setItems(data["items"]);
       } catch (error) {
         setError("Error fetching items");
       } finally {
@@ -63,7 +77,12 @@ const AllItemList = () => {
   return (
     <div className="flex flex-wrap flex-row gap-4 justify-center">
       {currentItems.map((item) => (
-        <ItemPreview key={item.nameId} item={item} />
+        <ItemPreview
+          key={item.nameId}
+          item={item}
+          inPortfolio={namesInPortfolio.includes(item.name)}
+          isLoggedIn={isLoggedIn}
+        />
       ))}
       <div>
         <Pagination>
@@ -83,7 +102,7 @@ const AllItemList = () => {
               </PaginationItem>
             ))}
             <PaginationItem>
-              <PaginationNext onClick={handleNextPage} href="#"/>
+              <PaginationNext onClick={handleNextPage} href="#" />
             </PaginationItem>
           </PaginationContent>
         </Pagination>
