50e26d7443829cd8527467ab90601c6d628076db
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-12-29 16:24:51 +0100

    feat: implement login/logout functionality in SideNav

--- frontend/app/ui/nav/sidenav.tsx
+++ frontend/app/ui/nav/sidenav.tsx
@@ -2,36 +2,24 @@
 import Link from 'next/link';
 import NavLinks from '@/app/ui/nav/nav-links';
 import SomeLogo from '@/app/ui/nav/some-logo';
-import { PowerIcon } from '@heroicons/react/24/outline';
+import { PowerIcon,ArrowRightEndOnRectangleIcon } from '@heroicons/react/24/outline';
 import { useRouter } from 'next/navigation';
-import { clearTokens } from '@/services/authServise';
+import { getToken, logOut } from '@/services/authServise';
+import { useState } from 'react';
 
 export default function SideNav() {
   const router = useRouter();
-
-  const handleLogout = async () => {
-    console.log('logging out...');
-    const refreshToken = localStorage.getItem('refresh_token');
-    const accessToken = localStorage.getItem('access_token');
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(!!getToken());
   
-    if (refreshToken) {
-      const response = await fetch('http://127.0.0.1:8000/api/logout/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${accessToken}`, // Dodano nagłówek autoryzacji
-        },
-        body: JSON.stringify({ refresh: refreshToken }),
-      });
-  
-      if (response.ok) {
-        clearTokens();
-        router.push('/login');
-      } else {
-        console.error('Logout failed with status:', response.status);
-      }
-    } else {
-      console.log('No refresh token found. User might not be logged in.');
+  const handleLoginLogout = async () => {
+    if (isLoggedIn) {
+      await logOut();
+      setIsLoggedIn(false);
+      window.location.reload();
+      return;
+    }else{
+      
+      router.push("/login");
     }
   };
 
@@ -49,11 +37,12 @@ export default function SideNav() {
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-xl bg-gray-50 md:block"></div>
         <button
-          onClick={handleLogout}
+          onClick={handleLoginLogout}
           className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-xl bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
         >
-          <PowerIcon className="w-6" />
-          <div className="hidden md:block">Sign Out</div>
+          
+          {isLoggedIn ? <PowerIcon className="w-6" /> : <ArrowRightEndOnRectangleIcon className="w-6" />}
+          <div className="hidden md:block">{isLoggedIn ? "Sign Out" : "Sign In"}</div>
         </button>
       </div>
     </div>
--- frontend/services/authServise.ts
+++ frontend/services/authServise.ts
@@ -1,8 +1,43 @@
 export const clearTokens = () => {
-    localStorage.removeItem('access_token');
-    localStorage.removeItem('refresh_token');
+  localStorage.removeItem("access_token");
+  localStorage.removeItem("refresh_token");
 };
 
 export const getToken = () => {
-    return localStorage.getItem('access_token');
+  return localStorage.getItem("access_token");
+};
+
+export const logOut = async () => {
+  console.log("logging out...");
+  const refreshToken = localStorage.getItem("refresh_token");
+  const accessToken = localStorage.getItem("access_token");
+
+  if (refreshToken) {
+    const response = await fetch("http://127.0.0.1:8000/api/logout/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({ refresh: refreshToken }),
+    });
+
+    if (response.ok) {
+      clearTokens();
+    } else {
+      if (accessToken && response.status == 401) {
+        clearTokens();
+        console.log("Clearing expired tokens");
+        
+        return;
+      }
+      console.error("Logout failed with status:", response.status);
+    }
+  } else {
+    console.log("No refresh token found.");
+    if (accessToken) {
+      console.log("Clearing access token");
+      clearTokens();
+    }
+  }
 };
